a16fa1b59f0a614dfc7b66f78ca67b76fedcf316
Porter T Dansie <dansie.porter@gmail.com>
2024-01-12 18:09:24 -0700

    Added class for BishopMoves, still needs to be implemented

---
+++ shared/src/main/java/chess/BishopMoves.java
@@ -0,0 +1,12 @@
+package chess;
+
+public class BishopMoves extends ChessPieceMoves {
+    public BishopMoves(ChessBoard chessBoard, ChessPosition chessPosition) {
+        super(chessBoard, chessPosition);
+    }
+
+    @Override
+    public void findMoves() {
+
+    }
+}
