5410d8477decea4cec67db994f335eafd6b8b575
Porter T Dansie <dansie.porter@gmail.com>
2024-01-12 18:47:01 -0700

    fixed error where invalid position was being set throwing IllegalArgumentsException

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -16,27 +16,26 @@ public class PawnMoves extends ChessPieceMoves{
             endingRow = 1;
         }
         //positions to check later
+        ChessPosition front = null;
+        ChessPosition doubleFront = null;
+        ChessPosition leftDiagonal = null;
+        ChessPosition rightDiagonal = null;
         //No need to check forward positions, as a double forward can only happen on the first row, and a single forward would only result in an error on the last row, where a pawn cannot exist
-        ChessPosition front = new ChessPosition(position.getRow() + colorInt, position.getColumn());
-        ChessPosition doubleFront = new ChessPosition(position.getRow() + 2 * colorInt, position.getColumn());
-        ChessPosition leftDiagonal;
-        ChessPosition rightDiagonal;
+        front = new ChessPosition(position.getRow() + colorInt, position.getColumn());
+
+
         if (position.getColumn() > 1) {
             leftDiagonal = new ChessPosition(position.getRow() + colorInt, position.getColumn() - 1);
-        }
-        else {
-            leftDiagonal = null;
         }
 
         if (position.getColumn() < 8) {
             rightDiagonal = new ChessPosition(position.getRow() + colorInt, position.getColumn() + 1);
         }
-        else {
-            rightDiagonal = null;
-        }
 
         //check if pawn is in starting position and offer first move
         if (position.getRow() == startingRow) {
+
+            doubleFront = new ChessPosition(position.getRow() + 2 * colorInt, position.getColumn());
             //check that both destination and passing square are empty
             if (chessBoard.getPiece(front) == null && chessBoard.getPiece(doubleFront) == null) {
                 moves.add(new ChessMove(position, doubleFront));
