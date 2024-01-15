9508e69cadef265200a3343571f876d4659307e1
Porter T Dansie <dansie.porter@gmail.com>
2024-01-15 16:48:51 -0700

    Implemented BishopMoves. Passing all tests

--- shared/src/main/java/chess/BishopMoves.java
+++ shared/src/main/java/chess/BishopMoves.java
@@ -1,12 +1,87 @@
 package chess;
 
 public class BishopMoves extends ChessPieceMoves {
-    public BishopMoves(ChessBoard chessBoard, ChessPosition chessPosition) {
-        super(chessBoard, chessPosition);
+    public BishopMoves(ChessBoard board, ChessPosition position) {
+        super(board, position);
     }
 
     @Override
     public void findMoves() {
+        int curentRow = position.getRow();
+        int currentCol = position.getColumn();
 
+        //Upper-Right Diagonal
+        while (curentRow < 8 && currentCol < 8) {
+            curentRow++;
+            currentCol++;
+            ChessPosition nextPosition = new ChessPosition(curentRow, currentCol);
+            if (chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).pieceColor != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+
+        }
+
+        //Lower-Right DIagonal
+        curentRow = position.getRow();
+        currentCol = position.getColumn();
+        while (curentRow > 1 && currentCol < 8) {
+            curentRow--;
+            currentCol++;
+            ChessPosition nextPosition = new ChessPosition(curentRow, currentCol);
+            if (chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).pieceColor != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+
+        }
+
+        //Lower-Left Diagonal
+        curentRow = position.getRow();
+        currentCol = position.getColumn();
+        while (curentRow > 1 && currentCol > 1) {
+            curentRow--;
+            currentCol--;
+            ChessPosition nextPosition = new ChessPosition(curentRow, currentCol);
+            if (chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).pieceColor != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+
+        }
+
+        //Upper-Left Diagonal
+        curentRow = position.getRow();
+        currentCol = position.getColumn();
+        while (curentRow < 8 && currentCol > 1) {
+            curentRow++;
+            currentCol--;
+            ChessPosition nextPosition = new ChessPosition(curentRow, currentCol);
+            if (chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).pieceColor != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+
+        }
     }
 }
