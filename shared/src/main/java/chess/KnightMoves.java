b9bd39526c7ace75e16e269998ae46906e0d5d91
Porter T Dansie <dansie.porter@gmail.com>
2024-01-15 18:25:55 -0700

    Implemented KnightMoves. All tests passing

--- shared/src/main/java/chess/KnightMoves.java
+++ shared/src/main/java/chess/KnightMoves.java
@@ -1,12 +1,40 @@
 package chess;
 
-public class KnightMoves extends ChessPieceMoves{
+import java.util.ArrayList;
+
+public class KnightMoves extends ChessPieceMoves {
     public KnightMoves(ChessBoard board, ChessPosition position) {
         super(board, position);
     }
 
     @Override
     public void findMoves() {
+        ArrayList<ChessPosition> possiblePositions = new ArrayList<>();
 
+        for (int i = 0; i < 8; i++) {
+            try {
+                ChessPosition currentPosition = new ChessPosition(position.getRow() + KNIGHT_MOVES[i][0], position.getColumn() + KNIGHT_MOVES[i][1]);
+                possiblePositions.add(currentPosition);
+            } catch (IllegalArgumentException e) {
+                System.out.println("IllegalArgumentException " + e.getMessage());
+            }
+        }
+
+        for (ChessPosition possiblePosition : possiblePositions) {
+            if (chessBoard.getPiece(possiblePosition) == null) {
+                moves.add(new ChessMove(position, possiblePosition));
+            } else if (chessBoard.getPiece(possiblePosition).getTeamColor() != chessBoard.getPiece(position).getTeamColor()) {
+                moves.add(new ChessMove(position, possiblePosition));
+            }
+        }
     }
+
+    public static final int[][] KNIGHT_MOVES = {{2, -1},
+            {2, 1},
+            {1, 2},
+            {-1, 2},
+            {-2, 1},
+            {-2, -1},
+            {1, -2},
+            {-1, -2}};
 }
