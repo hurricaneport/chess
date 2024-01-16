bc3434bbba912c477d60d4732f54919595574416
Porter T Dansie <dansie.porter@gmail.com>
2024-01-15 17:33:35 -0700

    Added RookMoves class, passing all tests

--- shared/src/main/java/chess/RookMoves.java
+++ shared/src/main/java/chess/RookMoves.java
@@ -8,5 +8,76 @@ public class RookMoves extends ChessPieceMoves{
     @Override
     public void findMoves() {
 
+        //Forward Line
+        int currentRow = position.getRow();
+        int currentCol = position.getColumn();
+        while(currentRow < 8) {
+            currentRow++;
+            ChessPosition nextPosition = new ChessPosition(currentRow, currentCol);
+
+            if(chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).getTeamColor() != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+        }
+
+        //Right Line
+        currentRow = position.getRow();
+        currentCol = position.getColumn();
+        while(currentCol < 8) {
+            currentCol++;
+            ChessPosition nextPosition = new ChessPosition(currentRow, currentCol);
+
+            if(chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).getTeamColor() != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+        }
+
+        //Bottom Line
+        currentRow = position.getRow();
+        currentCol = position.getColumn();
+        while(currentRow > 1) {
+            currentRow--;
+            ChessPosition nextPosition = new ChessPosition(currentRow, currentCol);
+
+            if(chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).getTeamColor() != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+        }
+
+        //Left Line
+        currentRow = position.getRow();
+        currentCol = position.getColumn();
+        while(currentCol > 1) {
+            currentCol--;
+            ChessPosition nextPosition = new ChessPosition(currentRow, currentCol);
+
+            if(chessBoard.getPiece(nextPosition) != null) {
+                if (chessBoard.getPiece(nextPosition).getTeamColor() != chessBoard.getPiece(position).getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition));
+                }
+                break;
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition));
+            }
+        }
     }
 }
