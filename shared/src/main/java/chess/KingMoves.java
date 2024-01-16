210475579c7e26bafc62cd4af770365b25938ac8
Porter T Dansie <dansie.porter@gmail.com>
2024-01-15 18:08:06 -0700

    Implemented KingMoves. ALl tests passing

--- shared/src/main/java/chess/KingMoves.java
+++ shared/src/main/java/chess/KingMoves.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class KingMoves extends ChessPieceMoves{
     public KingMoves(ChessBoard board, ChessPosition position) {
         super(board, position);
@@ -7,6 +12,34 @@ public class KingMoves extends ChessPieceMoves{
 
     @Override
     public void findMoves() {
+        ArrayList<ChessPosition> possiblePositions = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            try {
+                ChessPosition currentPosition = new ChessPosition(position.getRow() + KING_MOVES[i][0], position.getColumn() + KING_MOVES[i][1]);
+                possiblePositions.add(currentPosition);
+            } catch (IllegalArgumentException e) {
+                System.out.println("IllegalArgumentException " + e.getMessage());
+            }
+        }
+
+        for (ChessPosition possiblePosition : possiblePositions) {
+            if (chessBoard.getPiece(possiblePosition) == null) {
+                moves.add(new ChessMove(position, possiblePosition));
+            } else if (chessBoard.getPiece(possiblePosition).getTeamColor() != chessBoard.getPiece(position).getTeamColor()) {
+                moves.add(new ChessMove(position, possiblePosition));
+            }
+        }
 
     }
+
+    public static final int[][] KING_MOVES = {{1,1},
+        {1,0},
+        {1,-1},
+        {0,-1},
+        {-1,-1},
+        {-1,0},
+        {-1,1},
+        {0,1}};
+
 }
