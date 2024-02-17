f2fea16a841c036f8f7e31b2ad532d43fa692ec4
Porter T Dansie <dansie.porter@gmail.com>
2024-02-16 18:20:21 -0700

    Implemented UserDAO's

--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -2,19 +2,30 @@ package dataAccess;
 
 import model.AuthData;
 
+import java.util.HashSet;
+import java.util.Objects;
+
 public class MemoryAuthDAO implements AuthDAO{
+
+    private HashSet<AuthData> authTable = new HashSet<>();
     @Override
     public AuthData getAuthData(String authToken) {
+        for (AuthData authData : authTable) {
+            if (Objects.equals(authData.authToken(), authToken)) {
+                return authData;
+            }
+        }
+
         return null;
     }
 
     @Override
     public void addAuthData(AuthData authData) {
-
+        authTable.add(authData);
     }
 
     @Override
     public void clear() {
-
+        authTable = new HashSet<>();
     }
 }
--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -2,20 +2,30 @@ package dataAccess;
 
 import model.UserData;
 
+import java.util.HashSet;
+
 public class MemoryUserDAO implements UserDAO{
+
+    HashSet<UserData> userTable = new HashSet<>();
 
     @Override
     public boolean addUser(UserData userData) throws DataAccessException {
-        return false;
+        return userTable.add(userData);
     }
 
     @Override
     public UserData getUser(String username) {
+        for (UserData userData : userTable) {
+            if (userData.username().equals(username)) {
+                return userData;
+            }
+        }
+
         return null;
     }
 
     @Override
     public void clear() {
-
+        userTable = new HashSet<>();
     }
 }
