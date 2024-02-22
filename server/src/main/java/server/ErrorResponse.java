03189413246b5f261236dac3f7b93cb9bf1ed2c1
Porter T Dansie <dansie.porter@gmail.com>
2024-02-22 14:41:59 -0700

    Response class for Errors. This can be replaced with my exception classes

--- server/src/main/java/server/ErrorResponse.java
+++ server/src/main/java/server/ErrorResponse.java
@@ -1,4 +1,7 @@
 package server;
 
 public record ErrorResponse(String message, Integer status) implements Response {
+    public static ErrorResponse stripStatus(ErrorResponse errorResponse) {
+        return new ErrorResponse(errorResponse.message(), null);
+    }
 }
